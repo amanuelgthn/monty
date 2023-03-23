a1bc17f4f6ab5e85aa42c06c1cb5ff5b43c362f4
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-03-23 10:23:35 +0800

    Create 0-main.c

---
+++ 0-main.c
@@ -0,0 +1,8 @@
+#include"lists.h"
+/**
+*main-main function for operation of stacks
+*@argc:number of arguments
+*@argv;arguments passed, opcode and element to be added
+**/
+int main(int argc, char(*argv[]
+{
