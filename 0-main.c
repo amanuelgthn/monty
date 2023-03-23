3a1e0351cc18fa811a771f66790275b18d620eba
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-03-23 11:12:31 +0800

    Update 0-main.c

--- 0-main.c
+++ 0-main.c
@@ -4,6 +4,22 @@
 *@argc:number of arguments
 *@argv;arguments passed, opcode and element to be added
 **/
-int main(int argc, char(*argv[]
+int main(int argc, char(*argv[]))
 {
-jj
+	stack_t *new;
+	new = NULL;
+	
+	if (argc < 3 || (argc == 3 && isdigit(atoi(argv[2]) == 0)))
+	{
+		printf("L<line_number>: usage: push integer\n");
+		exit(EXIT_FAILURE);
+	}
+	else 
+	{
+		if (argv[1] == "push")
+		{
+			push(new, atoi(argv[2]));
+		}
+		return (new->n);
+	}
+}
