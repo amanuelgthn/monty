97356debe016f34fc96aa34546b2f3bd037070be
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-03-23 17:12:29 +0800

    Create split_line.c

---
+++ split_line.c
@@ -0,0 +1,17 @@
+#include"monty.h"
+/**
+*str_split-function to split a string with a delimiter of space
+*@str:string to be splitted
+*Return:pointer to the splitted string
+**/
+char *str_split(char str)
+{
+	char delim[] = " ";
+	char *split = strtok(str, delim);
+	
+	while(str != NULL)
+	{
+		ptr = strtok(NULL, delim);
+	}
+	return (ptr);
+}
