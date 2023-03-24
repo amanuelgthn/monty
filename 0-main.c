efc0083898a4d40aee62ac253ea5b4792d2c0284
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-03-24 10:04:01 +0800

    Update 0-main.c

--- 0-main.c
+++ 0-main.c
@@ -34,6 +34,7 @@ int main(int argc, char(*argv[]))
 		while ((read = getline(&line, &len, file)) != -1)
 		{
 			split_line = str_split(line,&words);
+			printf("%s\n",split_line[0]);
 			if(strcmp(split_line[0], "push") == 0)
 			{
 				push(&new,atoi(split_line[1]));
