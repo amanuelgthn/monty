e0e1b3b51416b7e95cbeb201807babea57ae23a7
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-03-24 09:10:06 +0800

    Update split_line.c

--- split_line.c
+++ split_line.c
@@ -4,19 +4,44 @@
 *@str:string to be splitted
 *Return:pointer to the splitted string
 **/
-char **str_split(char *str)
+char **str_split(char *str, int *number_of_words)
 {
-	char delim[] = " ";
-	char *split ;
-	char *string[2];
-	int i = 0;
-	
-	split = strtok(str, delim);
-	while(split != NULL)
-	{
-		split = strtok(NULL, delim);
-		string[i] =split;
-		i++;
-	}
-	return (*string);
+    char *piece, **str_arr = NULL, *str_cpy = NULL;
+    int i = 0;
+
+    if (str == NULL)
+    {
+        return (NULL);
+    }
+    str_cpy = strdup (str);
+    piece = strtok(str_cpy, " ");
+    while (piece != NULL)
+    {
+        if ((*piece) == '\n')
+        {
+            piece = strtok(NULL, " ");
+            continue;
+        }
+        (*number_of_words)++;
+        piece = strtok(NULL, " ");
+    }
+
+    str_arr = (char **)malloc(sizeof(char *) * (*number_of_words));
+    piece = strtok(str, " ");
+    for (i = 0; piece != NULL; i++)
+    {
+        if ((*piece) == '\n')
+        {
+            piece = strtok(NULL, " ");
+            continue;
+        }
+        str_arr[i] = (char *)malloc(sizeof(char) * (strlen(piece) + 1));
+        strcpy(str_arr[i], piece);
+        piece = strtok(NULL, " ");
+    }
+
+    if (str_cpy)
+        free (str_cpy);
+
+    return (str_arr);
 }
