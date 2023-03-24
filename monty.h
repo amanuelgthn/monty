249b4a57a15c34a6c819034870dfde03cc079e51
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-03-24 09:07:52 +0800

    Update monty.h

--- monty.h
+++ monty.h
@@ -38,5 +38,5 @@ typedef struct instruction_s
         void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 void push(stack_t **stack, int data);
-char **str_split(char *str);
+char **str_split(char *str, int *number_of_words);
 #endif /* _MONTY_ */ 
