84d80f508cc7cdd2ef56d908105aae533b9a25b9
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-03-24 10:40:17 +0800

    Update 0-stack_pall.c

--- 0-stack_pall.c
+++ 0-stack_pall.c
@@ -1,15 +1,42 @@
-#include"monty.h"
-/**
-*pall-print all elemensts in a doubly linked list
-*@stack:a doubly linked list to be printed
-**/
-void pall(stack_t *stack)
-{
-	stack_t *ptr = stack;
+#include "monty.h"
 
-	while (ptr != NULL)
+int value;
+/**
+ * new_Node - create new node
+ * @n: is a value
+ * Return: new node
+ */
+stack_t *new_Node(int n)
+{
+	stack_t *new = NULL;
+
+	new = malloc(sizeof(stack_t));
+	if (new == NULL)
 	{
-		printf("%d\n", ptr->n);
-		ptr = ptr->next;
+		dprintf(STDERR_FILENO, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
 	}
+	new->n = n;
+	new->next = NULL;
+	new->prev = NULL;
+
+	return (new);
+}
+
+/**
+ * _push - push item
+ * @stack: is a parameter
+ * @line_number: is value
+ */
+void _push(stack_t **stack, unsigned int line_number)
+{
+	stack_t *new = NULL;
+	(void)line_number;
+
+	new = new_Node(value);
+
+	new->next = *stack;
+	if (*stack != NULL)
+		(*stack)->prev = new;
+	*stack = new;
 }
