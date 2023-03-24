b5f046cdd751db26b6f401838e76f0c56e306746
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-03-24 10:36:23 +0800

    Update 0_stack_push.c

--- 0_stack_push.c
+++ 0_stack_push.c
@@ -3,14 +3,16 @@
 *push-function to push an element to the stack
 *@data:element to be added to the stack
 **/
-void push(stack_t **stack, int data)
+void push(stack_t **stack, unsigned int data)
 {
 	stack_t *new;
+	(void)line_number;
 
 	new = malloc(sizeof(stack_t));
 	if (new == NULL)
 	{
-		printf("Stack overflow\n");
+		dprintf(STDERR_FILENO, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
 	}
 	new->n = data;
 	new->next = *(stack);
